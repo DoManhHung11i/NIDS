23b583c3d20376012a54d24ae02804e729498505
DoManhHung11i <hung151004@gmail.com>
2025-06-22 00:34:10 +0700

    Update mlp model

--- intrusion_detection_systems/models/all_models/mlp.py
+++ intrusion_detection_systems/models/all_models/mlp.py
@@ -1,63 +1,21 @@
-# ========================== Random Forest Classifier =========================
-#
-#                   Author:  Sergio Arroni Del Riego
-#
-# =============================================================================
+import torch
+import torch.nn as nn
 
-# ==================> Imports
-import shared.models.model as m
+class MLPModel(nn.Module):
+    def __init__(self, input_dim):
+        super(MLPModel, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 64)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(64, 2)
 
-from sklearn.neural_network import MLPClassifier
-
-
-# ==================> Classes
-class mlp(m.Model):
-    def __init__(self, x_train: list, y_train: list, x_test: list, y_test: list, dataset: str, seed: int) -> None:
-        """__init__
-
-        This method is used to initialize the MLP class.
-
-        Parameters:
-            x_train: Training data
-            y_train: Training labels
-            x_test: Test data
-            y_test: Test labels
-            dataset: Dataset name
-        Output:
-            None
-        """
-        super().__init__(x_train, y_train, x_test, y_test, dataset, seed=seed)
-        self.exe()
-
-    # Override
-    def expecific_model(self) -> object:
-        """expecific_model
-
-        This method is an override of the parent method for the case of the MLP model.
-
-        Output:
-            object: MLP model
-        """
-        mlp = MLPClassifier(
-            hidden_layer_sizes=(32),
-            max_iter=200,
-            verbose=False,
-            random_state=self.seed,
-            batch_size=200,
-            early_stopping=True,
-            activation='tanh',
-            solver='adam'
-        )
-        mlp.fit(self.x_train, self.y_train)
-        return mlp
-
-    # Override
-    def __str__(self) -> str:
-        """__str__
-
-        This method is used to return the name of the class.
-
-        Output:
-            str: Name of the class
-        """
-        return "mlp"
+    def forward(self, x):
+        if x.dim() == 3 and x.shape[2] == 1:
+            x = x.squeeze(2)
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        return x
